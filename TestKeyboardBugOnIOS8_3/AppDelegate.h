32c67d112aa03dd143ca9ac86a2c000442e461ca
ultragtx <ultragtx@gmail.com>
2015-04-24 16:42:23 +0800

    Initial Commit

---
+++ TestKeyboardBugOnIOS8_3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestKeyboardBugOnIOS8_3
+//
+//  Created by Xinrong Guo on 4/24/15.
+//  Copyright (c) 2015 FoOTOo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestKeyboardBugOnIOS8_3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestKeyboardBugOnIOS8_3
+//
+//  Created by Xinrong Guo on 4/24/15.
+//  Copyright (c) 2015 FoOTOo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
